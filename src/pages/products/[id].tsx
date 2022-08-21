3705843fe5e7f36af0a2d75b361657e2e3c64178
RubenChirino <rubenchch21@gmail.com>
2022-08-21 18:24:07 -0300

    Add Assets folder

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { useRouter } from "next/router";
 
+// Services
+import { useProduct } from "@/services/useProduct";
+
 function ProductDetail() {
   const router = useRouter();
   const { id } = router.query;
+
+  const { product, isLoading, isError } = useProduct(id as string | undefined);
+  console.log("🚀 ~ product", product);
 
   return <div>Product: {id}</div>;
 }
